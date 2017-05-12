9d695fc95dcb2f342c031d41ecd26fc620b6eeca
Kathy Lau <imobilep501@gmail.com>
2017-05-12 12:15:57 -0400

    add upload fxnality

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import os
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'dogs'
 
+UPLOAD_FOLDER = './static/images/'
+ALLOWED_EXTENSIONS = set(['java', 'py', 'rkt', 'nlogo'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+'''
+This is used by the until now not in use file upload functionallity
+'''
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+'''
+This is used by the until now not in use file upload functionallity
+'''
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+                               
 @app.route("/")
 def root():
     return render_template("index.html")
