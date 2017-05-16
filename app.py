08392729301202c60060011de2ba986d90ae50e4
eccentricayman <aahmed3@stuy.edu>
2017-05-15 21:43:52 -0400

    wip, but students can do stuff?

--- app.py
+++ app.py
@@ -1,14 +1,112 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-import os
+from flask_mail import Mail, Message
+from pymongo import MongoClient
 from werkzeug.utils import secure_filename
+from utils import utils, accounts
+
+#connect to mongo
+connection = MongoClient("localhost", 27017, connect = False)
+db = connection['database']
+students = db['students']
+teachers = db['teachers']
+
+#get secret data
+secrets = utils.getSecretData()
 
 app = Flask(__name__)
-app.secret_key = 'dogs'
+mail = Mail(app)
+app.secret_key = secrets['app-secret-key']
 
 UPLOAD_FOLDER = './static/images/'
 ALLOWED_EXTENSIONS = set(['java', 'py', 'rkt', 'nlogo'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#mail configs
+app.config['MAIL_SERVER'] ='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = secrets['email']
+app.config['MAIL_PASSWORD'] = secrets['email-password']
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+#registers student and adds to database, stills requires email verif.
+#sends verification email
+#returns false if not registered
+def registerStudent(email, email2, firstname, lastname, password1, password2):
+    #check if email/password + confirm email/password are same
+    if email1 != email2:
+        return False
+    if password1 != password2:
+        return False
+
+    #create verification link/profile link
+    verificationLink = utils.getVerificationLink()
+    
+    #list of people with the same email
+    alreadyRegistered = list(db.students.find( { 'email': email } ))
+    #checks if above is empty
+    if not(alreadyRegistered):
+        #send email here
+        message = Message()
+        message.recipients = [ email1 ]
+        message.subject = "Confirm Your StuyCS Code Review Account"
+        message.html = '''
+        <center>
+        <h1 style="font-weight: 500 ; font-family: Arial">StuyCS Code Review</h1>
+        <p style="font-weight: 500 ; font-family: Arial">Thanks for signing up for StuyCS Code Review! Please press the button below to verify your account.</p>
+        <br>
+        <a href="%s" style="padding: 5% ; text-decoration: none ; border: 1px solid black ; text-transform: uppercase ; font-weight: 500 ; font-family: Arial ; padding-left: 10% ; padding-right: 10%">Verify Email</a>
+        </center>
+        ''' % ("127.0.0.1:5000/verify/" + verificationLink)
+        mail.send(message)
+        
+        
+        #add to db here, only password is hashed
+        db.students.insert_one(
+            {
+                'email': email1,
+                'password': hash(password1),
+                'verified': False,
+                'verificationLink': verificationLink,
+                'groups': [],
+                'classes': [],
+                #these are file ids
+                'files': [],
+                'profile': {
+                    'name': firstname + " " + lastname
+                }
+            }
+        )
+        return True
+    return False
+
+@app.route("/")
+def root():
+    if 'user' in session:
+        #status will always be set if user's set
+        if 'status' == "student":
+            student = accounts.getStudent(session['user'])
+            if student:
+                #home page of classes
+                return render_template("home.html", name = student['profile']['name'], email = student['email'], classes = student['classes'])
+            else:
+                #cant find account, prolly error so force logout and go to login page
+                del session['user']
+                return render_template("index.html", message = "Account error. Please try logging in again.")
+        #wowee we got ourrselves a teacher on our hands
+        else:
+            teacher = accounts.getTeacher(session['user'])
+            if teacher:
+                #home page of classes / notifications
+
+@app.route("/studentHome")
+def studentHome():
+    pass
+
+@app.route("/teacherHome")
+def teacherHome():
+    pass
 
 #This is used by the until now not in use file upload functionallity
 def allowed_file(filename):
@@ -20,23 +118,7 @@ def allowed_file(filename):
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
-                               
-@app.route("/")
-def root():
-    if user in session:
-        return redirect(url_for('studentHome')) if session['status'] == 'student' else redirect(url_for('teacherHome'))
-    else:
-        return render_template("index.html")
 
-@app.route("/studentHome")
-def studentHome():
-    pass
-
-@app.route("/teacherHome")
-def teacherHome():
-    pass
-
-    
 if __name__ == "__main__":
     app.debug = True
     app.run()
--- utils/accounts.py
+++ utils/accounts.py
@@ -1,11 +1,68 @@
 from pymongo import MongoClient
+import random
+import string
 import hashlib
+import utils
 
-connection = MongoClient("localhost", 27017, connect = False)
+#connect to mongo
+connection = MongoClient("127.0.0.1")
 db = connection['database']
-collection = db['users']
+students = db['students']
+teachers = db['teachers']
+
+#get secret data
+secrets = utils.getSecretData()
 
 #hashes text, used for sensitive data
 def hash(text):
     return hashlib.sha256(text).hexdigest()
 
+#generate a random alphanumeric verification link / id
+def getVerificationLink():
+    link = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
+    #check if any other users have this id
+    check = db.students.count(
+        {
+            'verificationID': link
+        }
+    )
+    #if the id isn't unique, recursively run and get another id
+    if check:
+        link = getVerificationLink()
+        return link
+    else:
+        return link
+
+#finds account from email which is psuedo id since it's unique
+#returns false if failed, or if more than 1 acc with that email
+def getStudent(email):
+    count = db.students.count(
+        {
+            'email': email
+        }
+    )
+    if count != 1:
+        return False
+    else:
+        return db.students.find(
+            {
+                'email': email
+            }
+        )
+
+def getTeacher(email):
+    count = db.teachers.count(
+        {
+            'email': email
+        }
+    )
+    if count != 1:
+        return False
+    else:
+        return db.teachers.find(
+            {
+                'email': email
+            }
+        )
+    
+    
