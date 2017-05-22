fbf7fca637e563effdf598cf901764c66a8c6d98
Kathy Lau <imobilep501@gmail.com>
2017-05-22 09:33:24 -0400

    add more basic group stuff but incomplete

--- utils/groups.py
+++ utils/groups.py
@@ -12,6 +12,7 @@ classes = db['classes']
 groups = db['groups']
 
 #creates a group and adds to database
+#prob need more code
 def createGroup( studentEmail, assignmentName, groupName ):
     teacher = list(db.teachers.find( {'email':teacherEmail} ))[0]
     code = createClassCode()
@@ -21,3 +22,25 @@ def createGroup( studentEmail, assignmentName, groupName ):
             'members': [studentEmail],
             'assignments': [],
               })
+
+
+#add a single student to class
+def addToGroup( studentEmail, assignmentName, groupName ):
+    db.groups.update(
+        {'name': groupName },
+        {'$push':
+         { 'members': studentEmail }
+        })
+    db.students.update(
+        {'email': studentEmail },
+        {'$push':
+         { 'groups': assignmentName + '-' + groupName }
+        })
+        ##important
+        #code to update db.classes
+
+#get data of a group
+def getGroup( name ):
+    return db.groups.find(
+        {'name': name }
+    )
